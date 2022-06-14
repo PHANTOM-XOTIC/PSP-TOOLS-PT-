cd87141b3d2221199fc5392521a63dabe2f7389f
PHANTOM XOTIC <106108205+PHANTOM-XOTIC@users.noreply.github.com>
2022-06-14 08:34:07 +0430

    Create apkinstall.h

---
+++ Apkinstall/apkinstall.h
@@ -0,0 +1,37 @@
+#include <apkinstall.cpp>
+با استفاده از فایل فضای نام؛
+استفاده از فضای نام android11;
+int apkinstall(){
+
+    مسیر اصلی فایل If And getChannel();
+    main findUntil();
+    مسیر if bool 30;
+    بازگشت 0;
+}
+{
+    نقشه Hash New<arry listmap1>;
+    listmap1.getBytes("apkfile");
+    listmap1.getType(.apk");
+    listmap1.name("getpathfile");
+    
+}
+{
+    get.path(prvoid.xml);
+    file.java.io (intent)
+    intent intent ();
+    intent startsWith(com.opntanist.intallapp)
+    intent start();
+    
+}
+{
+    if ("eluston"("showapp"))
+    bool C;
+    نصب برنامه؛
+    }الس{
+        showmsg("نصب Apk");
+
+ int 0 ;
+ HASH << LISTARR INC >> get.listmap(i++);
+}
+    }
+};
