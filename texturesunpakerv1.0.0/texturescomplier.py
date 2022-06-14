3cb5ea98677796cfd971ea7328f8d4bb0aa38fe7
PHANTOM XOTIC <106108205+PHANTOM-XOTIC@users.noreply.github.com>
2022-06-14 08:46:37 +0430

    Create texturescomplier.py

---
+++ texturesunpakerv1.0.0/texturescomplier.py
@@ -0,0 +1,20 @@
+import "os"
+import "object"
+import "object3d"
+import "windowssys"
+import "sys"
+	 
+	 {if else ( object = 0)
+	 from file object add for
+	 ( object = pngFile )
+	 exserat zim file = pass path()
+	 	else 
+		 ues add or bool 0
+		 return 10,
+		 ( "Fuke add dimrty add assert")
+		 }
+		 {
+		 string = pa ,
+		 @or i++ 0,
+		 return 0,
+		 }
